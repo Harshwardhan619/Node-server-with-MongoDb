82e820cdf1d32b94972ecf112f9762507bbeabef
Harshwardhan@619 <hmourya2001@gmail.com>
2020-06-30 16:21:43 +0530

    facebook auth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://172.18.64.1:27017/conFusion'
+    'mongoUrl' : 'mongodb://172.18.64.1:27017/conFusion',
+    'facebook' : {
+        'clientId' : '495117721263188',
+        'clientSecret' : '124b52cbfb67e1f4209d3914e200b4eb'
+    }
 }
